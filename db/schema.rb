04bf316143a6eee16b734c58e41e6a3aa7651925
Beau Waldrop <bmwaldrop@gmail.com>
2016-01-27 03:09:56 +0000

    Created and fixed issues with DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124202722) do
+ActiveRecord::Schema.define(version: 20160126031506) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
